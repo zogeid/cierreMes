bf5affd85078bcae52dda7bfa9fd447f55973062
dalares <zogeid@gmail.com>
2022-07-24 11:42:58 +0200

    2

--- cierreMes.py
+++ cierreMes.py
@@ -19,23 +19,45 @@ class Pep:
         print('MOB: ', self.mob, '\n')
 
 
-def get_month():
-    return{
-        1: 'Enero',
-        2: 'F',
-        3: 'M',
-        4: 'A',
-        5: 'M',
-        6: 'Junio',
-        7: 'Julio',
-        8: 'A',
-        9: 'S',
-        10: 'O',
-        11: 'N',
-        12: 'D',
-    }[datetime.datetime.now().month]
+# devuelve el mes actual
+def get_current_month():
+    return 'Mayo'
+    # return{
+    #     1: 'Enero',
+    #     2: 'Febrero',
+    #     3: 'Marzo',
+    #     4: 'Abril',
+    #     5: 'Mayo',
+    #     6: 'Junio',
+    #     7: 'Julio',
+    #     8: 'Agosto',
+    #     9: 'Septiembre',
+    #     10: 'Octubre',
+    #     11: 'Noviembre',
+    #     12: 'Diciembre',
+    # }[datetime.datetime.now().month]
 
 
+# devuelve la columna del mes actual
+def get_current_month_column():
+    return 'H'
+    # return{
+    #     1: 'D',
+    #     2: 'E',
+    #     3: 'F',
+    #     4: 'G',
+    #     5: 'H',
+    #     6: 'I',
+    #     7: 'J',
+    #     8: 'K',
+    #     9: 'L',
+    #     10: 'M',
+    #     11: 'N',
+    #     12: 'O',
+    # }[datetime.datetime.now().month]
+
+
+# devuelve el año actual
 def get_current_year():
     return datetime.datetime.now().year
 
@@ -51,11 +73,15 @@ def leer_simulacion():
     col = 'A'
     peps = []
 
+    # recorre Simulacion.xlsx para obtener los diferentes PEPs y guardarlos en peps
     for row in range(2, max_row):
         cell_pep = sheet[col + row.__str__()].value
         if cell_pep is not None and cell_pep not in peps:
             peps.append(cell_pep)
 
+    # Para cada PEP recoge datos de concepto, mob, nombre
+    # En funcion del concepto acumula los importes de Ingreso, Coste y MOB
+    # Con esta información genera un objeto PEP y lo añade a filled_peps
     for i in peps:
         ingreso_por_recurso_propio = 0
         coste_por_recurso_propio = 0
@@ -69,16 +95,16 @@ def leer_simulacion():
                 cell_concepto = sheet['AK' + row.__str__()].value
                 cell_mob = sheet['AJ' + row.__str__()].value
                 cell_nombre = sheet['B' + row.__str__()].value
-                cell_valor = sheet['AR' + row.__str__()].value
+                cell_importe = sheet['AR' + row.__str__()].value
 
                 if cell_concepto is not None:
                     if 'INGRESO POR RECURSO PROPIO' in cell_concepto:
-                        ingreso_por_recurso_propio = ingreso_por_recurso_propio + cell_valor
+                        ingreso_por_recurso_propio = ingreso_por_recurso_propio + cell_importe
                     if 'COSTE POR RECURSO PROPIO' in cell_concepto:
-                        coste_por_recurso_propio = coste_por_recurso_propio + cell_valor
+                        coste_por_recurso_propio = coste_por_recurso_propio + cell_importe
                 if cell_mob is not None:
                     if 'MOB-' in cell_mob:
-                        mob = mob + cell_valor
+                        mob = mob + cell_importe
                 if cell_nombre is not None:
                     nombre_proyecto = cell_nombre
 
@@ -95,15 +121,27 @@ def grabar_frp():
         filename_pep = filename[filename.find('D-'):28]
         for p in filled_peps:
             if p.pep == filename_pep:
-        #for p in filled_peps: # en vez de for, sacar el valor concreto de filled_peps
-        #   if p.pep in filename:
                 if p.pep == 'D-01350.1.1.1':
                     print("reca")
                     wb = load_workbook(filename)
                     sheet = wb.active
-                    print(get_month(), get_current_year())
-                    if get_current_year() == 2022:  # row26
-                        sheet['F26'].value = get_month()
+                    if get_current_year() == 2021:  # row2
+                        sheet['F2'].value = get_current_month()
+                    elif get_current_year() == 2022:  # row26
+                        sheet['F26'].value = get_current_month()
+
+                        suma_mano_obra = 0
+                        for i in range(29, 36):
+                            try:
+                                suma_mano_obra = suma_mano_obra + (sheet[f'C{i}'].value * sheet[f'{get_current_month_column()}{i}'].value)
+                            except:
+                                pass
+                        print(sheet['H42'].internal_value)
+                        if round(suma_mano_obra, 0) == round(p.coste, 0):  # Comprobamos que los costes coinciden
+                            sheet[f'{get_current_month_column()}41'].value = p.mob
+                            sheet[f'{get_current_month_column()}46'].value = p.ingreso
+
+                    wb.save(filename)
                     break
 
                 if p.pep == 'D-01362.1.1.1':
@@ -112,6 +150,7 @@ def grabar_frp():
                     sheet = wb.active
                     if 'D-01362.1.1.1' in sheet['C43'].value:
                         print ('ok')
+                    break
 
                 if p.pep == 'D-10168.1.1.1':
                     print("rgpd")
@@ -119,6 +158,7 @@ def grabar_frp():
                     sheet = wb.active
                     if 'D-10168.1.1.1' in sheet['C43'].value:
                         print ('ok')
+                    break
 
                 if p.pep == 'D-12330.1.1.1':
                     print("webm")
@@ -126,8 +166,9 @@ def grabar_frp():
                     sheet = wb.active
                     if 'D-12330.1.1.1' in sheet['C43'].value:
                         print ('ok')
+                    break
 
 
 leer_simulacion()
-#for r in filled_peps: r.__repr__()
+for r in filled_peps: r.__repr__()
 grabar_frp()
